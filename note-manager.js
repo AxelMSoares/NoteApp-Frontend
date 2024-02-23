e4b4807693b771ea3fff4007c80da9376b544abb
Axel <>
2024-02-23 13:41:01 +0100

    First commit of the noteapp frontend

---
+++ note-manager.js
@@ -0,0 +1,29 @@
+import { Note } from './note.js';
+
+export class NoteManager {
+    static async list() {
+        const response = await fetch('http://localhost:3000/notes/');
+        const data = await response.json();
+        return data.map(obj => new Note(obj.id, obj.text));
+    }
+
+    static async create(note) {
+        console.log('create for note: ', note);
+        const response = await fetch('http://localhost:3000/notes/', {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(note)
+        });
+    }
+
+    static async remove (id) {
+        const response = await fetch('http://localhost:3000/notes/' + id, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+    }
+}
